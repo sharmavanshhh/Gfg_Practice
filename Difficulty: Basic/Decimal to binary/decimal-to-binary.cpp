e95cd8bf2358f0e27ef43f143317c9ceb7f46432
VANSH SHARMA <sharmavansh0512@gmail.com>
2025-06-27 13:50:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Decimal to binary/decimal-to-binary.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    string decToBinary(int n) {
+        // code here
+        string ans = "";
+        while(n > 0){
+            int temp = n % 2;
+            char bit = temp + '0';
+            ans += bit;
+            n /= 2;
+        }
+        
+        reverse(ans.begin(), ans.end());
+        
+        return ans;
+    }
+};
