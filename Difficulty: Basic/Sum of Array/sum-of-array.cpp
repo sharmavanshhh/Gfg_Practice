6ac1db973f9a06da30a5f3978949670353467408
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-02-17 20:22:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sum of Array/sum-of-array.cpp
+++ Difficulty: Basic/Sum of Array/sum-of-array.cpp
@@ -2,17 +2,14 @@
 class Solution {
   public:
     // Function to return sum of elements
-    
-    int sum(vector<int>& arr, int n){
-        if(n == 0){
-            return 0;
-        }
-        return arr[n-1] + sum(arr, n-1);
-    }
-    
     int arraySum(vector<int>& arr) {
         // code here
-        int n = arr.size();
-        return sum(arr, n);
+       return sum(arr, arr.size());
+    }
+    
+    int sum(vector<int>& arr, int n){
+        if(n == 0) return 0;
+        
+        return arr[n-1] + sum(arr, n-1);
     }
 };
