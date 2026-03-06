7b160fee1f19fde5501350189325f5a720b1daa7
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-06 19:45:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
@@ -3,30 +3,27 @@ class Solution {
     int aggressiveCows(vector<int> &stalls, int k) {
         // code here
         sort(stalls.begin(), stalls.end());
-        int low = 1;
+        
+        int low = 0;
         int high = stalls.back() - stalls.front();
         
         while(low <= high){
             int mid = low + (high - low) / 2;
             
-            int cows = checkCows(stalls, mid);
+            int cows = canPlace(stalls, mid);
             
-            if(cows >= k){
-                low = mid + 1;
-            }
-            else{
-                high = mid - 1;
-            }
+            if(cows >= k) low = mid + 1;
+            else high = mid - 1;
         }
+        
         return high;
     }
     
-    int checkCows(vector<int>& stalls, int dist){
+    int canPlace(vector<int>& stalls, int dist){
         int cows = 1;
         int last = stalls[0];
         
-        
-        for(int i=1; i<stalls.size(); i++){
+        for(int i = 1; i < stalls.size(); i++){
             if(stalls[i] - last >= dist){
                 cows++;
                 last = stalls[i];
