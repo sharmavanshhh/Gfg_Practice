cd64703ddfcd1f4b7475607fdb158ec4ff6b0a31
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-12 14:06:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Max Sum without Adjacents/max-sum-without-adjacents.cpp
+++ Difficulty: Medium/Max Sum without Adjacents/max-sum-without-adjacents.cpp
@@ -4,18 +4,18 @@ class Solution {
     // calculate the maximum sum with out adjacent
     int findMaxSum(vector<int>& arr) {
         // code here
-        if(arr.size() <= 2){
-            return max(arr[0], arr[1]);
-        }
+        int n = arr.size();
+        if(n == 0) return 0;
+        if(n == 1) return arr[0];
+        if(n == 2) return max(arr[0], arr[1]);
         
-        int* dp = new int[arr.size()+1];
+        vector<int> dp(n);
         dp[0] = arr[0];
         dp[1] = max(arr[0], arr[1]);
-        
-        for(int i=2; i<=arr.size(); i++){
-            dp[i] = max(dp[i-1], dp[i-2] + arr[i]);
+        for(int i = 2; i < n; i++){
+            dp[i] = max(dp[i - 1], arr[i] + dp[i - 2]);
         }
         
-        return dp[arr.size()-1];
+        return dp[n - 1];
     }
 };
