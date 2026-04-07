5e428ef31a6c9e55bbc36d09d1000d48595ce18e
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-04-07 20:05:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -0,0 +1,43 @@
+class Solution {
+  public:
+    bool isCyclic(int V, vector<vector<int>> &edges) {
+        // code here
+                vector<vector<int>> adj(V);
+        
+        for(auto &e : edges){
+            int u = e[0];
+            int v = e[1];
+            adj[u].push_back(v);
+        }
+        
+        vector<int> indegree(V, 0);
+        for(int i = 0; i < V; i++){
+            for(int v : adj[i]){
+                indegree[v]++;
+            }
+        }
+        
+        queue<int> q;
+        for(int i = 0; i < V; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        
+        int count = 0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            count++;
+            
+            for(int neigh : adj[node]){
+                indegree[neigh]--;
+                if(indegree[neigh] == 0){
+                    q.push(neigh);
+                }
+            }
+        }
+        
+        return count != V;    
+    }
+};
