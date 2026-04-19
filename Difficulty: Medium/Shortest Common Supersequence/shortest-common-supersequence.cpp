070e35f32e2229d20a03c3050813c569a7989609
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-04-19 21:20:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
+++ Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
@@ -1,26 +1,22 @@
-// User function template for C++
-
 class Solution {
   public:
-    // Function to find length of shortest common supersequence of two strings.
-    int shortestCommonSupersequence(string &s1, string &s2) {
+    int minSuperSeq(string &s1, string &s2) {
         // code here
-        int m = s1.length();
-        int n = s2.length();
-
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-
-        for(int i=1; i<=m; i++){
-            for(int j=1; j<=n; j++){
+        int m = s1.size();
+        int n = s2.size();
+        
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
                 if(s1[i-1] == s2[j-1]){
                     dp[i][j] = dp[i-1][j-1] + 1;
                 }
                 else{
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
-
-        return m+n - dp[m][n];
+        return m + n - dp[m][n];
     }
 };
